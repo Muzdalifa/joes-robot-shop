7786e70661978d9b5733fcb4b2c15af929527509
Muzdalifa Ali <muzdalifa.ali@addgile.com>
2025-05-26 02:46:02 +0200

    posting data to a server

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { IProduct } from './catalog/product.model';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -7,10 +8,15 @@ import { IProduct } from './catalog/product.model';
 export class CartService {
    cart: IProduct[] = [];
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   add(product: IProduct){
+  //simple adding data to cartList
   this.cart.push(product)
-  console.log(`product ${product.name} added to cart`)
+  //post data to server
+  this.http.post("/api/cart", this.cart)
+    .subscribe(()=>{
+      console.log(`product ${product.name} added to cart`)
+    });  
  }
 }
